0a31ae041ad802fcc5ab4ba4a6b00b2639e433ed
Ousmane001 <diakitealphaousmane1@gmail.com>
2025-05-09 17:33:22 +0200

    polygone modifié et optim

--- implem/ei_implementation.h
+++ implem/ei_implementation.h
@@ -5,91 +5,148 @@
  * 
  */
 
-#ifndef EI_IMPLEMENTATION_H
-#define EI_IMPLEMENTATION_H
+ #ifndef EI_IMPLEMENTATION_H
+ #define EI_IMPLEMENTATION_H
+ 
+ #include "hw_interface.h"
+ #include "ei_types.h"
+ #include "ei_widget.h"
+ 
+ 
+typedef struct segment
+{
+	float ymax;
+	float x_ymin;
+	float pente;
+	struct segment* next;
+}segment;
 
-#include "hw_interface.h"
-#include "ei_types.h"
-#include "ei_widget.h"
+typedef  struct  TC_line_table
+{
+	segment* head;
+	segment* tail;
+}TC_line_table;
+
+typedef  struct TC_t
+{
+	uint32_t scan_line;
+	TC_line_table *head_of_TC;
+} TC_t;
 
 
+void draw_point(uint32_t* pixel_ptr, ei_size_t dimension, ei_point_t point, ei_color_t* color);
 
-/**
- * \brief	A structure storing the placement parameters of a widget.
- *		You have to define this structure: no suggestion provided.
- */
-struct ei_impl_placer_params_t;
+bool est_dans_clipper(ei_point_t* point, const ei_rect_t* clipper);
 
+TC_t* construire_TC(ei_point_t* point_array, uint32_t point_array_size, int y_min, int y_max);
 
+void supprime_cote_ymax_atteint(TC_line_table* TCA, uint32_t scanline);
 
-/**
- * \brief	Tells the placer to recompute the geometry of a widget.
- *		The widget must have been previsouly placed by a call to \ref ei_place.
- *		Geometry re-computation is necessary for example when the text label of
- *		a widget has changed, and thus the widget "natural" size has changed.
- *
- * @param	widget		The widget which geometry must be re-computed.
- */
-void ei_impl_placer_run(ei_widget_t widget);
+TC_line_table* tri_line_table(TC_line_table* table);
 
+void affiche_pixel_scanline(ei_surface_t surface, TC_line_table* TCA, uint32_t scanline, ei_color_t* color, const ei_rect_t* clipper);
 
+void maj_en_x_ymin(TC_line_table* TCA);
 
-/**
- * \brief	Fields common to all types of widget. Every widget classes specializes this base
- *		class by adding its own fields.
- */
-typedef struct ei_impl_widget_t {
-	ei_widgetclass_t*	wclass;		///< The class of widget of this widget. Avoids the field name "class" which is a keyword in C++.
-	uint32_t		pick_id;	///< Id of this widget in the picking offscreen.
-	ei_color_t		pick_color;	///< pick_id encoded as a color.
-	void*			user_data;	///< Pointer provided by the programmer for private use. May be NULL.
-	ei_widget_destructor_t	destructor;	///< Pointer to the programmer's function to call before destroying this widget. May be NULL.
+void ajouter_segment(TC_t *TC, ei_point_t point1, ei_point_t point2, int y_min);
 
-	/* Widget Hierachy Management */
-	ei_widget_t		parent;		///< Pointer to the parent of this widget.
-	ei_widget_t		children_head;	///< Pointer to the first child of this widget.	Children are chained with the "next_sibling" field.
-	ei_widget_t		children_tail;	///< Pointer to the last child of this widget.
-	ei_widget_t		next_sibling;	///< Pointer to the next child of this widget's parent widget.
-
-	/* Geometry Management */
-	ei_impl_placer_params_t* placer_params;	///< Pointer to the placer parameters for this widget. If NULL, the widget is not currently managed and thus, is not displayed on the screen.
-	ei_size_t		requested_size;	///< See \ref ei_widget_get_requested_size.
-	ei_rect_t		screen_location;///< See \ref ei_widget_get_screen_location.
-	ei_rect_t*		content_rect;	///< See ei_widget_get_content_rect. By defaults, points to the screen_location.
-} ei_impl_widget_t;
+segment* creer_segment(ei_point_t point1, ei_point_t point2);
 
 
 
-/**
- * @brief	Draws the children of a widget.
- * 		The children are draw withing the limits of the clipper and
- * 		the widget's content_rect.
- *
- * @param	widget		The widget which children are drawn.
- * @param	surface		A locked surface where to draw the widget's children.
- * @param	pick_surface	The picking offscreen.
- * @param	clipper		If not NULL, the drawing is restricted within this rectangle
- *				(expressed in the surface reference frame).
- */
-void		ei_impl_widget_draw_children	(ei_widget_t		widget,
-						 ei_surface_t		surface,
-						 ei_surface_t		pick_surface,
-						 ei_rect_t*		clipper);
 
 
-
-/**
- * \brief	Converts the red, green, blue and alpha components of a color into a 32 bits integer
- * 		than can be written directly in the memory returned by \ref hw_surface_get_buffer.
- * 		The surface parameter provides the channel order.
- *
- * @param	surface		The surface where to store this pixel, provides the channels order.
- * @param	color		The color to convert.
- *
- * @return 			The 32 bit integer corresponding to the color. The alpha component
- *				of the color is ignored in the case of surfaces that don't have an
- *				alpha channel.
- */
-uint32_t	ei_impl_map_rgba(ei_surface_t surface, ei_color_t color);
-
-#endif
+  /**
+   * @brief Colorie les pixels entre origine et extrémité en respectant l'algorithme de Bresenham
+   *  
+   * @param origine Point de départ à colorier /origine de la ligne
+   * @param extremite Point suivant à colorier / extrémité de la ligne
+   * @param color Pointeur vers la couleur avec laquelle on va colorier
+   * @param
+   *  
+   */
+ void algo_Bresenham(ei_point_t origine, ei_point_t extremite, ei_color_t* color, uint32_t* pixel_ptr, ei_size_t dimension, const ei_rect_t* clipper);
+ 
+ 
+ 
+ 
+ /**
+  * \brief	A structure storing the placement parameters of a widget.
+  *		You have to define this structure: no suggestion provided.
+  */
+ struct ei_impl_placer_params_t;
+ 
+ 
+ 
+ /**
+  * \brief	Tells the placer to recompute the geometry of a widget.
+  *		The widget must have been previsouly placed by a call to \ref ei_place.
+  *		Geometry re-computation is necessary for example when the text label of
+  *		a widget has changed, and thus the widget "natural" size has changed.
+  *
+  * @param	widget		The widget which geometry must be re-computed.
+  */
+ void ei_impl_placer_run(ei_widget_t widget);
+ 
+ 
+ 
+ /**
+  * \brief	Fields common to all types of widget. Every widget classes specializes this base
+  *		class by adding its own fields.
+  */
+ typedef struct ei_impl_widget_t {
+	 ei_widgetclass_t*	wclass;		///< The class of widget of this widget. Avoids the field name "class" which is a keyword in C++.
+	 uint32_t		pick_id;	///< Id of this widget in the picking offscreen.
+	 ei_color_t		pick_color;	///< pick_id encoded as a color.
+	 void*			user_data;	///< Pointer provided by the programmer for private use. May be NULL.
+	 ei_widget_destructor_t	destructor;	///< Pointer to the programmer's function to call before destroying this widget. May be NULL.
+ 
+	 /* Widget Hierachy Management */
+	 ei_widget_t		parent;		///< Pointer to the parent of this widget.
+	 ei_widget_t		children_head;	///< Pointer to the first child of this widget.	Children are chained with the "next_sibling" field.
+	 ei_widget_t		children_tail;	///< Pointer to the last child of this widget.
+	 ei_widget_t		next_sibling;	///< Pointer to the next child of this widget's parent widget.
+ 
+	 /* Geometry Management */
+	 //ei_impl_placer_params_t* placer_params;	///< Pointer to the placer parameters for this widget. If NULL, the widget is not currently managed and thus, is not displayed on the screen.
+	 ei_size_t		requested_size;	///< See \ref ei_widget_get_requested_size.
+	 ei_rect_t		screen_location;///< See \ref ei_widget_get_screen_location.
+	 ei_rect_t*		content_rect;	///< See ei_widget_get_content_rect. By defaults, points to the screen_location.
+ } ei_impl_widget_t;
+ 
+ 
+ 
+ /**
+  * @brief	Draws the children of a widget.
+  * 		The children are draw withing the limits of the clipper and
+  * 		the widget's content_rect.
+  *
+  * @param	widget		The widget which children are drawn.
+  * @param	surface		A locked surface where to draw the widget's children.
+  * @param	pick_surface	The picking offscreen.
+  * @param	clipper		If not NULL, the drawing is restricted within this rectangle
+  *				(expressed in the surface reference frame).
+  */
+ void		ei_impl_widget_draw_children	(ei_widget_t		widget,
+						  ei_surface_t		surface,
+						  ei_surface_t		pick_surface,
+						  ei_rect_t*		clipper);
+ 
+ 
+ 
+ /**
+  * \brief	Converts the red, green, blue and alpha components of a color into a 32 bits integer
+  * 		than can be written directly in the memory returned by \ref hw_surface_get_buffer.
+  * 		The surface parameter provides the channel order.
+  *
+  * @param	surface		The surface where to store this pixel, provides the channels order.
+  * @param	color		The color to convert.
+  *
+  * @return 			The 32 bit integer corresponding to the color. The alpha component
+  *				of the color is ignored in the case of surfaces that don't have an
+  *				alpha channel.
+  */
+ uint32_t	ei_impl_map_rgba(ei_surface_t surface, ei_color_t color);
+ 
+ #endif
+ 
--- tests/lines.c
+++ tests/lines.c
@@ -24,6 +24,18 @@ void test_line(ei_surface_t surface, ei_rect_t* clipper)
 	ei_draw_polyline(surface, pts, sizeof(pts) / sizeof(ei_point_t), color, clipper);
 }
 
+/**
+ * @brief fonction qui afficher le clipper du main histoire de bien
+ * 			voir les bors de celuici 
+ */
+void test_rectangle(ei_surface_t surface, ei_rect_t* clipper)
+{
+	ei_color_t		color		= { 0, 0, 0, 255 };
+	ei_point_t		pts[5]		= { {200, 150}, {600, 150}, {600, 450}, {200, 450},  {200, 150}};
+
+	ei_draw_polyline(surface, pts, sizeof(pts) / sizeof(ei_point_t), color, clipper);
+}
+
 
 
 /* test_octogone --
@@ -59,6 +71,7 @@ void test_octogone(ei_surface_t surface, ei_rect_t* clipper)
 
 	/* Draw the shape with polylines */
 	ei_draw_polyline(surface, pts, sizeof(pts) / sizeof(ei_point_t), (ei_color_t){0, 255, 0, 255}, clipper);
+	ei_draw_polygon(surface, pts, sizeof(pts) / sizeof(ei_point_t), (ei_color_t){0, 255, 0, 255}, clipper);
 }
 
 
@@ -94,6 +107,8 @@ void test_square(ei_surface_t surface, ei_rect_t* clipper)
 
 	/* Draw the shape with polylines */
 	ei_draw_polyline(surface, pts, sizeof(pts) / sizeof(ei_point_t), color, clipper);
+	printf("renteemnt\n");
+	ei_draw_polygon(surface, pts,sizeof(pts) / sizeof(ei_point_t), color, clipper);
 }
 
 
@@ -113,7 +128,85 @@ void test_dot(ei_surface_t surface, ei_rect_t* clipper)
 }
 
 
+/*test_polygone 
+ * On dessine un polygone et puis on essaye de le colorier
+*/
+void test_polygone(ei_surface_t surface, ei_rect_t* clipper){
+	printf("debut polygone\n");
+    ei_color_t color = {255,0,255,255};
+	ei_color_t color1 = {0,0,0,255};
+    ei_point_t pts[5] ={{10,10},{700,100},{700,500},{10,400},{10,10}};
+    // On dessine maintenant le polygone
+	printf("avant poline\n");
+    ei_draw_polyline(surface,pts,sizeof(pts)/sizeof(ei_point_t),color1,clipper);
+	printf("poliline fait\n");
+	ei_draw_polygon(surface,pts,sizeof(pts)/sizeof(ei_point_t), color,clipper);
+}
 
+/*test_arc
+ * j'essaie de dessiner un arc avec polyline
+ * 
+*/
+// void test_arc(ei_surface_t surface, ei_rect_t* clipper){
+// 	ei_color_t color = {0,250,0,255};
+// 	ei_point_t* point_arc=arc(0,0,0,1.57)
+// 	ei_draw_polyline(surface,point_arc,sizeof(pts)/sizeof(ei_point_t),color,NULL);
+// }
+
+/*test_drapeau_maroc 
+ * On essaye de dessiner le drapeau du maroc
+*/
+
+void drapeau_maroc(ei_surface_t surface, ei_rect_t* clipper) {
+    // 1. Dessiner le fond rouge
+    ei_color_t rouge = { 17, 38, 206, 255};  // Rouge du drapeau marocain
+    ei_rect_t drapeau_rect = {{200, 150}, {400, 300}}; // Rectangle du drapeau
+    ei_fill(surface, &rouge, &drapeau_rect);
+
+    // 2. Dessiner l'étoile verte
+    ei_color_t vert = {0, 128, 0, 255};  // Vert du drapeau marocain
+    
+    // Points de l'étoile ordonnés (ABCDA)
+    ei_point_t etoile[6] = {
+        {400, 250},      // A (point de départ)
+        {450, 300},      // B
+        {400, 350},      // C
+        {350, 300},      // D
+        {400, 250}       // A (retour au point de départ)
+    };
+
+    // Dessiner le contour de l'étoile (ABCDA)
+    //ei_draw_polyline(surface, etoile, 5, vert, clipper);
+
+    // Dessiner le remplissage de l'étoile
+    //ei_draw_polygon(surface, etoile, 5, vert, clipper);
+
+    // Dessiner les lignes intérieures (diagonales)
+    ei_point_t diagonales[6] = {
+        {400, 200},      // A vers C
+        {300, 400},      // C
+        {500, 266},      // B vers D
+        {300, 266},      // D
+        {500, 400},      // A (pour fermer)
+        {400, 200}       // C (dernière diagonale)
+    };
+	ei_point_t diagonale2[6] = {
+    {390, 190},      // A vers C
+    {290, 390},      // C
+    {490, 256},      // B vers D
+    {290, 256},      // D
+    {490, 390},      // A (pour fermer)
+    {390, 190}       // C (dernière diagonale)
+};
+
+
+    ei_draw_polyline(surface, diagonales, 6, vert, clipper);
+    //ei_draw_polyline(surface, diagonale2, 6, vert, clipper);
+
+}
+// ei_draw_polygon(surface,pentagram,sizeof(pentagram)/sizeof(ei_point),vert,clipper);
+	
+// 	};
 /*
  * ei_main --
  *
@@ -125,8 +218,8 @@ int main(int argc, char** argv)
 	ei_surface_t		main_window	= NULL;
 	ei_color_t		white		= { 0xff, 0xff, 0xff, 0xff };
 	ei_rect_t*		clipper_ptr	= NULL;
-//	ei_rect_t		clipper		= ei_rect(ei_point(200, 150), ei_size(400, 300));
-//	clipper_ptr		= &clipper;
+	ei_rect_t		clipper		= ei_rect(ei_point(200, 150), ei_size(400, 300));
+	clipper_ptr		= &clipper;
 	ei_event_t		event;
 
 	hw_init();
@@ -138,10 +231,16 @@ int main(int argc, char** argv)
 	ei_fill		(main_window, &white, clipper_ptr);
 
 	/* Draw polylines. */
-	test_line	(main_window, clipper_ptr);
-	test_octogone	(main_window, clipper_ptr);
-	test_square	(main_window, clipper_ptr);
-	test_dot	(main_window, clipper_ptr);
+	//test_line	(main_window, clipper_ptr);
+	//test_octogone	(main_window, clipper_ptr);
+	 //test_square	(main_window, clipper_ptr);
+	//test_dot	(main_window, clipper_ptr);
+	// on affiche le clipper pour voir si le clipping est bon : 
+	//test_rectangle(main_window, NULL);
+	printf("avant de rentrer\n");
+	//test_polygone(main_window,clipper_ptr);
+	drapeau_maroc(main_window,NULL);
+
 	
 	/* Unlock and update the surface. */
 	hw_surface_unlock(main_window);
